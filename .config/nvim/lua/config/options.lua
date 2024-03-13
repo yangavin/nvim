71bf80dd623d96a24af447cc2ec1c373011ea69f
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-03-12 23:36:32 -0400

    i trust the sensible lazyvim defaults ig

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -8,11 +8,6 @@ opt.scrolloff = 8
 opt.number = true
 opt.relativenumber = true
 
--- Shift related options
-opt.tabstop = 4
-opt.softtabstop = 4
-opt.shiftwidth = 4
-
 -- Highlight yank
 vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function()
