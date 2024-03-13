7751ca5823c514d55d1357fa7af4838d90d13f43
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-03-13 15:09:48 -0400

    remove github theme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,3 @@
 return {
-  {
-    "projekt0n/github-nvim-theme",
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      require("github-theme").setup({
-        -- options = {
-        --   transparent = true,
-        -- },
-      })
-    end,
-  },
-  {
-    "catppuccin/nvim",
-  },
+  "catppuccin/nvim",
 }
