a3c84265552339a4e8509d3eddb062cde4175926
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2024-03-13 02:12:11 -0400

    add copilot and resolve <Tab> conflict

---
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -0,0 +1,37 @@
+-- Manually set snippet tab keymap due to copilot conflict
+-- When presented with both, snippet always wins
+return {
+  {
+    "L3MON4D3/LuaSnip",
+    -- Disable default <Tab> keybindings
+    keys = function()
+      return {}
+    end,
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
+
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
+        if luasnip.expand_or_jumpable() then
+          luasnip.jump(1)
+        else
+          -- Let copilot handle the rest
+          fallback()
+        end
+      end, { "i", "s" })
+
+      opts.mapping["<S-Tab>"] = cmp.mapping(function(fallback)
+        if luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, { "i", "s" })
+    end,
+  },
+}
